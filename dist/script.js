7d4b2d18ab1314e07f668f0ca8b057a7bdb902f6
Andre <pandu_si1@outlook.com>
2025-01-01 23:03:50 +0800

    baru

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,11 @@
 onload = () => {
   const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    document.getElementById("anv").classList.remove("d-none");
     const audioPlayer = document.getElementById("audioPlayer");
     audioPlayer.play(); // Memutar lagu
+
+    document.body.classList.remove("not-loaded");
+    document.getElementById("anv").classList.remove("d-none");
+
     clearTimeout(c);
   }, 1000);
 };
