fc67fd6250415bed325b4e0436f5119c967d82b4
Andre <pandu_si1@outlook.com>
2025-01-01 22:58:29 +0800

    baru

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,7 @@
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
+    document.getElementById("anv").classList.remove("d-none");
     const audioPlayer = document.getElementById("audioPlayer");
     audioPlayer.play(); // Memutar lagu
     clearTimeout(c);
